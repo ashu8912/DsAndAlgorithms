2f2ff6455301bd76b20cec7d46248d7afcd76e91
ashu8912 <ashughildiyal5@gmail.com>
2019-01-07 13:48:30 +0530

    finding max Sum naive way

---
+++ max_sum_naive.js
@@ -0,0 +1,18 @@
+function max_sum_naive(arr,num){
+let maxSum=-Infinity;
+let sum=0;
+for(let i=0;i<arr.length-num+1;i++)
+{
+    for(let j=i;j<num+i;j++)
+    {
+       sum=sum+arr[j];
+    }
+    if(sum>maxSum)
+    {
+        maxSum=sum;
+    }
+    sum=0;
+}
+return maxSum;
+}
+console.log(max_sum_naive([1,2,3,4,5,,8,6,7],2));

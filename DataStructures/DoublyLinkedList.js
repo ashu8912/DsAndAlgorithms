a0c9a758e6565df397c6bfa0a485ac5120724e6e
ashu8912 <ashughildiyal5@gmail.com>
2019-02-15 14:40:22 +0530

    pop,shift and unshift implemented on doublyLinkedList

--- DataStructures/DoublyLinkedList.js
+++ DataStructures/DoublyLinkedList.js
@@ -26,10 +26,91 @@ class DoublyLinkedList{
         this.length++;
         return this;
     }
-    
+    pop(){
+        
+        let temp=this.tail;   
+        if(!this.head)
+        {
+            return undefined;
+        }
+        
+        else
+        if(this.length===1)
+        {
+            this.head=null;
+            this.tail=null;
+        }
+        else{
+       this.tail.prev.next=null;
+       this.tail=temp.prev;
+       temp.prev=null;
+     }
+     this.length--;
+     return temp;   
+    }
+    shift(){
+        let temp=this.head;
+        if(!this.head)
+        {
+            return undefined;
+        }
+        if(this.length===1)
+        {
+            this.head=null;
+            this.tail=null;
+        }else
+        {this.head=temp.next;
+        this.head.prev=null;
+        temp.next=null;
+        }
+        this.length--;
+        return temp;
+    }
+    unshift(val){
+     let newNode=new Node(val);
+     if(!this.head)
+     {
+         this.head=newNode;
+         this.tail=this.head;
+     }else{
+         this.head.prev=newNode;
+         newNode.next=this.head;
+         this.head=newNode;
+     }
+     this.length++;
+     return newNode;
+    }
+    get(index){
+        if(index>=this.length || index<0) return null;
+        let midIndex=Math.floor(this.length/2);
+        let counter,current;
+        if(index<=midIndex)
+        {
+           counter=0;
+           current=this.head;
+           while(counter!=index)
+           {
+               counter++;
+               current=current.next;
+           } 
+        }else{
+            
+          counter=this.length-1;
+          current=this.tail;
+          while(counter!=index)
+          {
+           counter--;
+           current=current.prev;
+          }
+          
+        }
+        return current;
+    }
 }
 let list=new DoublyLinkedList();
 list.push("ashu");
 list.push("ghildiyal");
 list.push("champ");
-console.log(list);
+list.shift();
+list.unshift("harry potter");
+console.log(list.get(1));

c793b26da4cbb7cc20daa662f42da2de47981511
ashu8912 <ashughildiyal5@gmail.com>
2019-02-15 09:02:25 +0530

    push method added to doublyLinkedList

---
+++ DataStructures/DoublyLinkedList.js
@@ -0,0 +1,35 @@
+class Node{
+    constructor(val){
+        this.val=val;
+        this.prev=null;
+        this.next=null;
+    }
+}
+class DoublyLinkedList{
+    constructor(){
+        this.head=null;
+        this.tail=null;
+        this.length=0;
+    }
+    push(val)
+    {
+        let newNode=new Node(val);
+        if(!this.head)
+        {
+            this.head=newNode;
+            this.tail=this.head;
+        }else{
+        this.tail.next=newNode;
+        newNode.prev=this.tail;
+        this.tail=newNode;
+        }
+        this.length++;
+        return this;
+    }
+    
+}
+let list=new DoublyLinkedList();
+list.push("ashu");
+list.push("ghildiyal");
+list.push("champ");
+console.log(list);
